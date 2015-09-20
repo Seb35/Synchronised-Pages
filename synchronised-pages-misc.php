691e95a14c0ccf5e23f5779d4700e89e2ea7db5f
Seb35 <seb35wikipedia@gmail.com>
2015-09-20 19:49:54 +0200

    Version 0.1.1 Cleaned first Alpha Release
    Cleaning:
    - Re-organised functions accross files; it should no more move from now
      - The main file has require_once to sub-files by theme and has calls to actions/filters
    - Verified indentation and changed all spaces to tabulations
    - Improved doc of each function and inside functions
    - Checked XSS for translations
    
    Features:
    - You can no more create tags in the dedicated taxonomy from the interface (well, you
      can if you manipulate the DOM, it’s only hidden with CSS)
    - Created a POT file that the translators can copy to start a translation in a given language
      - Added translator hints for each message in the code
      - Created with makepot.php
      - Some messages were manually moved, so that the translation flow is more logic/thematic
      - Manually removed the "translators: " prefix in the POT file
    - Synchronised the translation fr_FR with the POT file and translated the hints to fr_FR if a
      translator prefers to translate from French instead of from English

--- synchronised-pages-misc.php
+++ synchronised-pages-misc.php
@@ -1,245 +1,211 @@
 <?php
+/**
+ * @package Synchronised Pages
+ * @version 0.1.1
+ * @license WFTPL 2.0
+ */
+
 
 /**
  * Register the new status (Template) and the taxonomy Synchronised pages
+ *
+ * @action init
  */
 function synchronised_pages_add_stati() {
-    
-    // Register a custom post status for the templates: these are privately-published posts in a final form (not drafts)
-    // The main advandage over the Private core status is you have a tab on the top of the post list, to avoid mixing
-    // really private posts and template posts. The drawback of such a custom status is that, when you deactivate the
-    // extension, the templates are no more in the post list, although you can access them through their post id.
-	register_post_status( 'template', array(
-		'label'        => __( 'Template', 'synchronised-pages' ),
-		'private'      => true,
-		'label_count'  => _n_noop( 'Template <span class="count">(%s)</span>', 'Templates <span class="count">(%s)</span>', 'synchronised-pages' ),
-	) );
-    
-    // Labels of the taxonomy below
-	$labels = array(
-		'name'              => __( 'Genres', 'synchronised-pages' ),
-		'singular_name'     => __( 'Genre', 'synchronised-pages' ),
-		'search_items'      => __( 'Search Genres', 'synchronised-pages' ),
-		'all_items'         => __( 'All Genres', 'synchronised-pages' ),
-		'parent_item'       => __( 'Parent Genre', 'synchronised-pages' ),
-		'parent_item_colon' => __( 'Parent Genre:', 'synchronised-pages' ),
-		'edit_item'         => __( 'Edit Genre', 'synchronised-pages' ),
-		'update_item'       => __( 'Update Genre', 'synchronised-pages' ),
-		'add_new_item'      => __( 'Add New Genre', 'synchronised-pages' ),
-		'new_item_name'     => __( 'New Genre Name', 'synchronised-pages' ),
-		'menu_name'         => __( 'Genre', 'synchronised-pages' ),
-	);
 	
-    // Register the taxonomy where are grouped the imports of synchronised pages
-    register_taxonomy( 'synchronised_pages', null, array(
-   		'label'        => __( 'Synchronised pages', 'synchronised-pages' ),
-    //	'labels'       => $labels,
-    	'description'  => __( 'Sets of synchronised pages from a template page', 'synchronised-pages' ),
-    	'hierarchical' => true,
-    	'public'       => false,
-        'show_ui'      => true,
-    ) );
-    
-    // Get post types where we want synchronised pages
-    $post_types = get_option( 'synchronised-pages-setting-post-types', null );
-    if( $post_types ) $post_types = array_keys( (array) $post_types );
-    else if( $post_types === null ) $post_types = array( 'page' );
-    else $post_types = array();
-    
-   	// Register taxonomy for each requested post type
-    foreach( $post_types as $post_type ) {
-        
-        register_taxonomy_for_object_type( 'synchronised_pages', $post_type );
-    }
-}
-add_action( 'init', 'synchronised_pages_add_stati' );
-
-
-function synchronised_pages_add_admin() {
-    
-    register_setting( 'writing', 'synchronised-pages-setting-post-types', 'synchronised_pages_validate_settings_post_types' );
-    
-    register_setting( 'writing', 'synchronised-pages-setting-display-synchronised', 'boolval' );
-    
-    add_settings_section(
-        'synchronised-pages',
-        __('Synchronised Pages', 'synchronised-pages'),
-        'synchronised_pages_settings',
-        'writing'
-    );
-    
-    add_settings_field(
-		'synchronised-pages-post-types',
-		__('Post Types', 'synchronised-pages'),
-		'synchronised_pages_settings_post_types',
-		'writing',
-		'synchronised-pages'
-	);
-    
-    add_settings_field(
-		'synchronised-pages-display-synchronised',
-		__('Synchronised status', 'synchronised-pages'),
-		'synchronised_pages_settings_display_synchronised',
-		'writing',
-		'synchronised-pages'
-	);
-}
-add_action( 'admin_init', 'synchronised_pages_add_admin' );
-
-function synchronised_pages_settings( $arg ) {
-    
-    echo esc_html__('Selected post types will be given the possibility to be mass-generated. Relevant taxonomies will be created.', 'synchronised-pages');
-}
-
-function synchronised_pages_settings_post_types() {
-    
-    $post_types = get_post_types( array( 'public' => true ), 'objects' );
-    $post_types_setting = get_option( 'synchronised-pages-setting-post-types', null );
-    if( $post_types_setting ) $post_types_setting = array_keys( (array) $post_types_setting );
-    else if( $post_types_setting === null ) $post_types_setting = array( 'page' );
-    else $post_types_setting = array();
-    
-    $first = true;
-    foreach( $post_types as $post_type ) {
-        
-        if( $post_type->name == 'attachment' ) continue; // This type is too complicated for now as it must be handled differently
-        if( !$first ) echo '<br />';
-        echo '<input name="synchronised-pages-setting-post-types['.$post_type->name.']" id="synchronised-pages-checkbox-'.$post_type->name.'" type="checkbox" class="code" value=""'.checked( in_array($post_type->name, $post_types_setting), true, false ).' /> <label for="synchronised-pages-checkbox-'.$post_type->name.'">'.esc_html($post_type->label).'</label>';
-        $first = false;
-    }
-}
-
-function synchronised_pages_validate_settings_post_types( $input ) {
-    
-    $post_types = get_post_types( array( 'public' => true ) );
-    $post_types_setting = array_keys( (array) $input );
-    
-    foreach( $post_types_setting as $post_type ) {
-        
-        if( ! in_array( $post_type, $post_types ) ) {
-            
-            add_settings_error( 'synchronised-pages-setting-post-types', 'invalid-post-types', sprintf( esc_html__('Some of the values you entered in %s → %s are not post types.', 'synchronised-pages' ), '<i>'.__('Synchronised Pages', 'synchronised-pages').'</i>', '<i>'.__('Post Types', 'synchronised-pages').'</i>' ) );
-            return get_option( 'synchronised-pages-setting-post-types', '' );
-        }
-        
-        if( $post_type == 'attachment' ) {
-            
-            add_settings_error( 'synchronised-pages-setting-post-types', 'unavailable-post-type', sprintf( esc_html__('For now, it is not possible to use the post type %s in %s → %s. Possibly in a future version of the plugin %s.', 'synchronised-pages'), '<i>'.get_post_type_object('attachment')->label.'</i>', '<i>'.__('Synchronised Pages', 'synchronised-pages').'</i>', '<i>'.__('Post Types', 'synchronised-pages').'</i>', '<i>'.__('Synchronised Pages', 'synchronised-pages').'</i>' ) );
-            return get_option( 'synchronised-pages-setting-post-types', '' );
-        }
-    }
-    
-    return $input;
-}
-
-function synchronised_pages_settings_display_synchronised() {
-    
-    $display_synchronised_setting = get_option( 'synchronised-pages-setting-display-synchronised', true );
-    
-    echo '<input name="synchronised-pages-setting-display-synchronised" id="synchronised-pages-setting-display-synchronised" type="checkbox" class="code" value="1"'.checked( $display_synchronised_setting, true, false ).' /> <label for="synchronised-pages-setting-display-synchronised">'.__('Display the status &#8220;Synchronised&#8221; in the post list', 'synchronised-pages').'</label>';
+	// Register a custom post status for the templates: these are privately-published posts in a final form (not drafts)
+	// The main advandage over the Private core status is you have a tab on the top of the post list, to avoid mixing
+	// really private posts and template posts. The drawback of such a custom status is that, when you deactivate the
+	// extension, the templates are no more in the post list, although you can access them through their post id.
+	register_post_status( 'template', array(
+		// translators: New post status
+		'label'		=> __( 'Template', 'synchronised-pages' ),
+		'private'	=> true,
+		// translators: Label of the link to the specific post status
+		'label_count'=> _n_noop( 'Template <span class="count">(%s)</span>', 'Templates <span class="count">(%s)</span>', 'synchronised-pages' ),
+	) );
+	
+	// Labels of the taxonomy below
+	//$labels = array(
+	//	'name'			=> __( 'Genres', 'synchronised-pages' ),
+	//	'singular_name'	 => __( 'Genre', 'synchronised-pages' ),
+	//	'search_items'	=> __( 'Search Genres', 'synchronised-pages' ),
+	//	'all_items'		 => __( 'All Genres', 'synchronised-pages' ),
+	//	'parent_item'	 => __( 'Parent Genre', 'synchronised-pages' ),
+	//	'parent_item_colon' => __( 'Parent Genre:', 'synchronised-pages' ),
+	//	'edit_item'		 => __( 'Edit Genre', 'synchronised-pages' ),
+	//	'update_item'	 => __( 'Update Genre', 'synchronised-pages' ),
+	//	'add_new_item'	=> __( 'Add New Genre', 'synchronised-pages' ),
+	//	'new_item_name'	 => __( 'New Genre Name', 'synchronised-pages' ),
+	//	'menu_name'		 => __( 'Genre', 'synchronised-pages' ),
+	//);
+	
+	// Register the taxonomy where are grouped the imports of synchronised pages
+	register_taxonomy( 'synchronised_pages', null, array(
+		// translators: New taxonomy
+ 		'label'		=> __( 'Synchronised pages', 'synchronised-pages' ),
+	//	'labels'	 => $labels,
+		// translators: Description of the taxonomy
+		'description'=> __( 'Sets of synchronised pages from a template page', 'synchronised-pages' ),
+		'hierarchical' => true,
+		'public'	 => false,
+		'show_ui'	=> true,
+	) );
+	
+	// Get post types where we want synchronised pages
+	$post_types = get_option( 'synchronised-pages-setting-post-types', null );
+	if ( $post_types ) $post_types = array_keys( (array) $post_types );
+	else if ( $post_types === null ) $post_types = array( 'page' );
+	else $post_types = array();
+	
+ 	// Register taxonomy for each requested post type
+	foreach ( $post_types as $post_type ) {
+		
+		register_taxonomy_for_object_type( 'synchronised_pages', $post_type );
+	}
 }
 
 
 /**
  * Append the status Template in the UI
  *
- * This part will be mostly cut when #12706 will be solved
+ * This part will be mostly cut when bug #12706 will be solved
+ *
+ * @action admin_footer-post.php
+ * @action admin_footer-post-new.php
  */
 function synchronised_pages_append_status() {
-     global $post;
-     $complete_template = '';
-     $complete_synchronised = '';
-     $label = '';
-     
-     $post_types = get_option( 'synchronised-pages-setting-post-types', null );
-     if( $post_types ) $post_types = array_keys( (array) $post_types );
-     else if( $post_types_setting === null ) $post_types_setting = array( 'page' );
-     else $post_types = array();
-     
-     if( in_array( $post->post_type, $post_types ) || $post->post_status == 'template' ) {
-          if( $post->post_status == 'template' ) {
-              $complete_template = ' selected="selected"';
-              $label = ' <span id="post-status-display">'.esc_js(esc_html__( 'Template', 'synchronised-pages' )).'</span>';
-          }
-          echo '
-          <script>
-          jQuery(document).ready(function($){
-               
-               // Add the status in the stati available for the user
-               $(\'select#post_status\').append(\'<option value="template"'.$complete_template.'>'.esc_js(esc_html__( 'Template', 'synchronised-pages' )).'</option>\');
-               $(\'.misc-pub-post-status label\').append(\''.$label.'\');
-               
-               // Save some original values – intentionally global variables
-               synchronised_pages_original_publish = $(\'#publish\').attr(\'name\');
-               synchronised_pages_original_publish_val = $(\'#publish\').val();
-               synchronised_pages_original_status = $(\'option:selected\', $(\'#post_status\')).val();
-               
-               var synchronised_pages_manage_status = function() {
-                   
-                   if( $(\'option:selected\', $(\'#post_status\')).val() == \'template\' ) {
-                   
-                       // Hide the \'Save draft\' button and change the \'Publish\' button to save
-                       $(\'#save-post\').hide();
-                       $(\'#publish\').attr(\'name\', \'save\');
-                       $(\'#publish\').val(synchronised_pages_original_status==\'template\'?\''.esc_js(esc_html__('Update the template', 'synchronised-pages')).'\':\''.esc_js(esc_html__('Publish the template', 'synchronised-pages')).'\');
-                       
-                   } else {
-                       
-                       // Restore the original \'Publish\' button, either save or publish depending on the current state – WP manages the \'Save\' button
-                       if( synchronised_pages_original_publish ) {
-                           $(\'#publish\').attr(\'name\', synchronised_pages_original_publish);
-                           $(\'#publish\').val(synchronised_pages_original_publish_val);
-                       }
-                   }
-               };
-               synchronised_pages_manage_status();
-               $(\'#post-status-select\').find(\'.save-post-status\').click( synchronised_pages_manage_status );
-               $(\'#post-status-select\').find(\'.cancel-post-status\').click( synchronised_pages_manage_status );
-          });
-          </script>
-          ';
-     }
+	
+	global $post;
+	
+	// Retrieve the registered post types to be synchronised
+	$post_types = get_option( 'synchronised-pages-setting-post-types', null );
+	if ( $post_types ) $post_types = array_keys( (array) $post_types );
+	else if ( $post_types_setting === null ) $post_types_setting = array( 'page' );
+	else $post_types = array();
+	
+	// The post type is made available for only registered post types, but also, as an exception,
+	// for existing Templates even after the post type becomes unregistered (NB: it should be tested
+	// if saving does work, possibly the 'template' status is automatically changed to 'publish' by WordPress in this case)
+	if ( ! in_array( $post->post_type, $post_types ) && $post->post_status != 'template' ) return;
+	
+	// Prepare variable JavaScript+HTML snippets
+	$template_label = esc_js( esc_html( __('Template', 'synchronised-pages') ) );
+	// translators: A button to save
+	$update_template_label = esc_js( esc_html( __('Update the template', 'synchronised-pages') ) );
+	// translators: A button to save
+	$publish_template_label = esc_js( esc_html( __('Publish the template', 'synchronised-pages') ) );
+	$complete = '';
+	$label = '';
+	if ( $post->post_status == 'template' ) {
+		$complete = ' selected="selected"';
+		$label = ' <span id="post-status-display">'.$template_label.'</span>';
+	}
+	
+	// Add this script since WordPress doesn’t automatically add the status in the user interface and no hook is available (see bug #12706)
+	echo '
+	<script>
+	jQuery(document).ready(function($){
+		
+		// Add the status in the stati available for the user
+		$(\'select#post_status\').append(\'<option value="template"'.$complete.'>'.$template_label.'</option>\');
+		$(\'.misc-pub-post-status label\').append(\''.$label.'\');
+		
+		// Save some original values
+		var synchronised_pages_original_publish = $(\'#publish\').attr(\'name\'),
+		    synchronised_pages_original_publish_val = $(\'#publish\').val(),
+		    synchronised_pages_original_status = $(\'option:selected\', $(\'#post_status\')).val();
+		
+		var synchronised_pages_manage_status = function() {
+			
+			if( $(\'option:selected\', $(\'#post_status\')).val() == \'template\' ) {
+				
+				// Hide the \'Save draft\' button and change the \'Publish\' button to save
+				$(\'#save-post\').hide();
+				$(\'#publish\').attr(\'name\', \'save\');
+				$(\'#publish\').val(synchronised_pages_original_status==\'template\'?\''.$publish_template_label.'\':\''.$update_template_label.'\');
+				
+			} else {
+				
+				// Restore the original \'Publish\' button, either save or publish depending on the current state – WP manages the \'Save\' button
+				if( synchronised_pages_original_publish ) {
+					$(\'#publish\').attr(\'name\', synchronised_pages_original_publish);
+					$(\'#publish\').val(synchronised_pages_original_publish_val);
+				}
+			}
+		};
+		synchronised_pages_manage_status();
+		$(\'#post-status-select\').find(\'.save-post-status\').click( synchronised_pages_manage_status );
+		$(\'#post-status-select\').find(\'.cancel-post-status\').click( synchronised_pages_manage_status );
+	});
+	</script>
+	';
 }
-add_action( 'admin_footer-post.php', 'synchronised_pages_append_status' );
-add_action( 'admin_footer-post-new.php', 'synchronised_pages_append_status' );
 
 
 /**
- * Display the state Template or Synchronised in the summary
+ * Remove the possibility to create a tag in the taxonomy synchronised_pages in the user interface
+ *
+ * @action admin_print_scripts-edit-tags.php
+ */
+function synchronised_pages_remove_tag_creation() {
+	
+	echo '<style type="text/css">
+	.taxonomy-synchronised_pages #col-left {
+		display: none;
+	}
+	.taxonomy-synchronised_pages #col-right {
+		width: 100%;
+	}
+</style>
+';
+}
+
+
+/**
+ * Display the state Template or Synchronised in the posts list
+ *
+ * @filter display_post_states
  */
 function synchronised_pages_display_state( $states ) {
-    
-    global $post;
-    $arg = get_query_var( 'post_status' );
-    
-    if( $arg != 'template' ) {
-        if( $post->post_status == 'template' ) {
-            return array( esc_html__('Template', 'synchronised-pages') );
-        }
-    }
-    if( has_term( '', 'synchronised_pages', $post->ID ) && get_option( 'synchronised-pages-setting-display-synchronised', true ) ) {
-        return array( esc_html__('Synchronised', 'synchronised-pages') );
-    }
-    return $states;
+	
+	global $post;
+	
+	// Display nothing in the 'Template' posts list
+	if ( get_query_var( 'post_status' ) == 'template' ) return $states;
+	
+	// Display the word 'Template' for such posts only in the 'All' posts list
+	if ( $post->post_status == 'template' ) {
+		return $states + array( esc_html( __('Template', 'synchronised-pages') ) );
+	}
+	
+	// Display the word 'Synchronised' in every posts list when a post has at least one term in the synchronised_pages taxonomy
+	if ( has_term( '', 'synchronised_pages', $post->ID ) && get_option( 'synchronised-pages-setting-display-synchronised', true ) ) {
+		// translators: A label in the posts list
+		return $states + array( esc_html( __('Synchronised', 'synchronised-pages') ) );
+	}
+	
+	return $states;
 }
-add_filter( 'display_post_states', 'synchronised_pages_display_state' );
 
 
 /**
  * Register the tool in the Tools menu
+ *
+ * @action admin_menu
  */
 function synchronised_pages_register_tool_submenu() {
-	add_management_page( esc_html__('Synchronised Pages', 'synchronised-pages'), esc_html__('Synchronised Pages', 'synchronised-pages'), 'manage_options', 'synchronised-pages', 'synchronised_pages_tool_page' );
-    //add_submenu_page( 'tools.php', esc_html__('Synchronised Pages', 'synchronised-pages'), esc_html__('Synchronised Pages', 'synchronised-pages'), 'manage_options', 'edit-tags.php?taxonomy=synchronised_pages&post_type=any' );
+	add_management_page( esc_html( __('Synchronised Pages', 'synchronised-pages') ), esc_html( __('Synchronised Pages', 'synchronised-pages') ), 'manage_options', 'synchronised-pages', 'synchronised_pages_tool_page' );
+	//add_submenu_page( 'tools.php', esc_html( __('Synchronised Pages', 'synchronised-pages') ), esc_html( __('Synchronised Pages', 'synchronised-pages') ), 'manage_options', 'edit-tags.php?taxonomy=synchronised_pages&post_type=any' );
 }
-add_action( 'admin_menu', 'synchronised_pages_register_tool_submenu' );
 
 
 /**
  * Load l10n
+ *
+ * @action plugins_loaded
  */
 function synchronised_pages_load_plugin_textdomain() {
-    load_plugin_textdomain( 'synchronised-pages', false, basename( dirname( __FILE__ ) ) . '/languages/' );
+	load_plugin_textdomain( 'synchronised-pages', false, basename( dirname( __FILE__ ) ) . '/languages' );
 }
-add_action( 'plugins_loaded', 'synchronised_pages_load_plugin_textdomain' );
 
--- synchronised-pages.php
+++ synchronised-pages.php
@@ -1,19 +1,69 @@
 <?php
 /**
  * @package Synchronised Pages
- * @version 0.1
+ * @version 0.1.1
+ * @license WFTPL 2.0
  */
 /*
 Plugin Name: Synchronised Pages
 Plugin URI: http://wordpress.org/plugins/synchronised-pages/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Description: This plugin generates a bunch of pages according to a template page with various informations extracted from a database. For instance you can mass-generate one page per event of a festival, with specific informations for each event.
 Author: Sébastien Beyou
-Version: 0.1
+Version: 0.1.1
 Author URI: https://www.seb35.fr
 */
 
+
+
+
+
+/**
+ * Add subpages by functional theme
+ */
 require_once "synchronised-pages-job.php";
-require_once "synchronised-pages-tool.php";
 require_once "synchronised-pages-misc.php";
-//require_once "synchronised-pages-settings.php";
+require_once "synchronised-pages-tool.php";
+require_once "synchronised-pages-settings.php";
+
+
+
+
+
+/**
+ * Register actions and filters
+ */
+
+/// Functions defined in synchronised-pages-misc.php
+
+// Register the new status (Template) and the taxonomy Synchronised pages
+add_action( 'init', 'synchronised_pages_add_stati' );
+
+// Append the status Template in the UI
+// TODO Add the same for Quick Edit
+add_action( 'admin_footer-post.php', 'synchronised_pages_append_status' );
+add_action( 'admin_footer-post-new.php', 'synchronised_pages_append_status' );
+
+// Remove the possibility to create a tag in the taxonomy synchronised_pages in the user interface
+add_action( 'admin_print_scripts-edit-tags.php', 'synchronised_pages_remove_tag_creation' );
+
+// Display the state Template or Synchronised in the posts list
+add_filter( 'display_post_states', 'synchronised_pages_display_state' );
+
+// Register the tool in the Tools menu
+add_action( 'admin_menu', 'synchronised_pages_register_tool_submenu' );
+
+// Load l10n
+add_action( 'plugins_loaded', 'synchronised_pages_load_plugin_textdomain' );
+
+
+/// Functions defined in synchronised-pages-settings.php
+
+// Register the settings and admin forms
+add_action( 'admin_init', 'synchronised_pages_add_admin' );
+
+
+/// Functions defined in synchronised-pages-tool.php
+
+// Catch the submit action after click on “Import”
+add_action( 'admin_post_synchronised_pages_import', 'synchronised_pages_import' );
 
